3ec47298a3d7488018b04feee0c0e5f5c275e7ed
NatalieGichuki <nataliegichuki@gmail.com>
2025-03-07 17:18:49 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,17 @@
+function receivesAFunction(spy){
+    return spy();                                                                                                                                                                                                    ``````````````````````````````````````````````````````````````````
+}
+
+
+
+function returnsANamedFunction(){
+    return function namedFunction(){
+        return 'I am a named function';
+    };
+}
+
+function returnsAnAnonymousFunction(){
+    return function(){
+        return 'I am an anonymous function';
+    };
+}
